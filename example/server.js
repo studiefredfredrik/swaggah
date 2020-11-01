935ea15b654c97738c63e5a96d335867f7e7ac3b
studiefredfredrik <studiefredfredrik@gmail.com>
2020-11-01 04:48:56 +0100

    wip, works

---
+++ example/server.js
@@ -0,0 +1,37 @@
+let express = require('express');
+
+let app = express();
+
+
+
+let config = {
+  port: process.env.PORT || 8080,
+}
+
+function startServer(){
+
+  app.get('/', (req, res) => {
+    res.send('Good morning 😍')
+  })
+  app.get('/test', (req, res) => {
+    res.send('Good morning 😍')
+  })
+  app.put('/test', (req, res) => {
+    res.send('Good morning 😍')
+  })
+  app.post('/test2', (req, res) => {
+    res.send('Good morning 😍')
+  })
+  app.get('/yolo55/{:id}', (req, res) => {
+    res.send('Good morning 😍')
+  })
+
+  let swagger = require('./swaggah');
+  swagger.register(app)
+
+
+  app.listen(config.port, '0.0.0.0')
+  console.log(`Served on http://localhost:${config.port}`)
+}
+
+startServer()
